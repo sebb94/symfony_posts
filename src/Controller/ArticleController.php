e398d7eeaf16575ab72430cd848886453829daf9
Seba <omgl0lwth@gmail.com>
2019-10-27 17:39:34 +0100

    some twig styles, tables with articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,10 +14,15 @@ class ArticleController extends Controller{
    * @Route("/home")
    * @Method({"GET"})
    */
+ 
 
     public function index(){
+
+         $articles = ['Art 1', 'Art 2'];
+
             return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticleController'
+            'controller_name' => 'ArticleController',
+            'atricles' => $articles
         ]);
     }
 
