4e52548c1afd082c890d0ac029097f475559aa6c
Seba <omgl0lwth@gmail.com>
2019-10-29 17:05:04 +0100

    create new article form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,7 +6,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method; 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends Controller{
 
@@ -24,6 +27,41 @@ class ArticleController extends Controller{
         ]);
     }
 
+
+     /** 
+   * @Route("/article/new", name="new_article")
+   * @Method({"GET", "POST"})
+   */
+
+   public function new( Request $request){
+
+    $article = new Article();
+    $form = $this->createFormBuilder($article)
+    ->add('title', TextType::class, array(
+        "attr" => array(
+            'class' => 'form-control'),
+
+    ))->add('body', TextareaType::class, array(
+        'required' => false,
+        "attr" => array(
+            'class' => 'form-control'),
+
+    ))->add('save', SubmitType::class, array(
+        'label' => "Create",
+        "attr" => array(
+            'class' => 'btn btn-primary mt-3',),     
+
+    ))->getForm();
+
+    return $this->render('articles/new.html.twig', [
+           'form' => $form->CreateView()
+        ]);
+
+
+
+   }
+
+   
   /** 
    * @Route("/article/{id}", name="article_show")
    * @Method({"GET"})
@@ -36,7 +74,6 @@ class ArticleController extends Controller{
         ]);
 
    }
-
 
       /** 
    * @Route("/article/save")
